4ac580d666fb275ddb5629010ad301b2949b24f1
Hyun_Gwang <mabr2845@gmail.com>
2022-08-06 19:56:59 +0900

    setting of reducer

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'antd/dist/antd.css';
+
+import { Provider } from 'react-redux';
+import PromisMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(PromisMiddleware, ReduxThunk)(createStore);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStoreWithMiddleware(Reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && 
+    window.__REDUX_DEVTOOLS_EXTENSION__())}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
